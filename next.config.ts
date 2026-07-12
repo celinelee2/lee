1c71cdccd437bce54251330b1d0e861183fe3201
Celine <celinelee.otto2@gmail.com>
2026-07-12 13:53:12 +0800

    chore: add PRISMA_GENERATE_SKIP_AUTOINSTALL to next.config, simplify build script

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  env: {
+    PRISMA_GENERATE_SKIP_AUTOINSTALL: "true",
+  },
+};
 
 export default nextConfig;
